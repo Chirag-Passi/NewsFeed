96b18e2c673cbc5ffcae34e3e0d7cfab95203df9
Chirag-Passi <chiragpassi1999@gmail.com>
2023-04-05 13:15:00 +0530

    API Working Fine

--- src/components/News.js
+++ src/components/News.js
@@ -4,27 +4,43 @@ import NewsItem from "../NewsItem";
 export class News extends Component {
   constructor() {
     super();
-    let articles = [];
+
+    console.log("This");
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
     };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=178ef43577074a1cb1934c87b740b96c";
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+    });
   }
 
   render() {
     return (
       <div className="container my-3">
-        <h2> My News Feed App</h2>
+        <h1> My News Feed App</h1>
         <div className="row">
           {this.state.articles.map((element) => {
-            <div className="col-md-4" key={element.url}>
-              <NewsItem
-                title={element.title.slice(0, 45)}
-                description={element.description.slice(0, 45)}
-                imageUrl={element.urlToImage}
-                newsUrl={element.url}
-              />
-            </div>;
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 45) : ""
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
           })}
         </div>
       </div>
